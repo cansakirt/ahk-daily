74381c5c27b236fc66969221c12bc5102b74d25a
cansakirt <cansakirt@gmail.com>
2018-09-10 19:45:05 -0400

    Create Print to PDF.bat

---
+++ Print to PDF.bat
@@ -0,0 +1,10 @@
+REM @ECHO OFF
+REM @ECHO "%~1"
+start AcroRd32.exe /t "%~1" "Microsoft Print to PDF"
+REM @pause
+
+REM AcroRd32.exe /n /t "C:\Users\<user>\Desktop\pdf.pdf" "Microsoft Print to PDF"
+REM AcroRd32.exe /h /n /o /s /t "C:\Users\<user>\Desktop\pdf.pdf" "Microsoft Print to PDF"
+REM AcroRd32.exe /A "zoom=50&navpanes=1=OpenActions&search=batch" "C:\Users\<user>\Desktop\pdf.pdf"
+
+
